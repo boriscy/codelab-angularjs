f48e2e0b0933b54d4fa4858e10bd62c653d5d11f
Boris Barroso <boriscyber@gmail.com>
2014-07-11 20:15:39 -0400

    Final

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,10 +1,65 @@
-'use strict';
-
 var app = angular.module('codelab', []);
+"use strict";
 
 app.controller('MasterController', function($scope) {
-  $scope.name = 'Violeta';
+  $scope.products = [];
 
+  // Calcula el total iterando los productos y sumandolos
+  $scope.total = function() {
+    sum = 0;
+    angular.forEach($scope.products, function(prod) {
+      sum += prod.price * prod.quantity;
+    });
+
+    return sum;
+  };
+
+  // Busca un ítem en la lista
+  findItem = function(id) {
+    var items = $scope.items.filter(function(it) { return it.id === id; });
+    console.log(items);
+    return items[0];
+  };
+
+  // Adiciona un nuevo producto a la lista de productos
+  $scope.addNew = function() {
+    // Definimos un nuevo producto usando la función $scope.$new
+    // debido a que nos proporciona métodos que necesitaremos
+    var prod = $scope.$new();
+
+    prod.quantity = 1;
+    // Observamos si se cambia el item
+    prod.$watch('item', function(newVal, oldVal) {
+      // Si el item es diferente entonces ponemos precio y detalles
+      if(angular.isNumber(newVal)) {
+        var item = findItem(newVal);
+
+        prod.id = item.id;
+        prod.price = item.price;
+        prod.name = item.name;
+        prod.unit = item.unit;
+        prod.price = item.price;
+      }
+    });
+    $scope.products.push(prod);
+  };
+
+
+  // Elimina un producto del array de productos
+  $scope.del = function(prod, index) {
+    $scope.products.splice(index, 1);
+    prod.$destroy();
+  };
+
+  // Presenta en este caso salva los productos
+  $scope.save = function() {
+    var prods = $scope.products.map(function(p) {
+      return {id: p.id, nombre: p.name, precio: p.price, cantidad: p.quantity};
+    });
+    $scope.text = prods;
+  };
+
+  // Lista de items
   $scope.items = [
     {id: 1, name: 'Carne de res', price: 35.5, unit: 'Kg.'},
     {id: 2, name: 'Vaso de vidrio', price: 10, unit: 'Un.'},
